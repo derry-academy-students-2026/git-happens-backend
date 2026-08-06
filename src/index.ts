61d6ef6f2d38aa675662010b861e0bd5e087e446
Cormac Mulvenna <cormac.mulvenna@kainos.com>
2026-08-06 13:47:15 +0100

    Us038 unit tests backend (#2)
    * created scaffold of backend with health endpoint
    
    * git ignore update
    
    * linter skip added
    
    * temporary test
    
    * Save work before deleting old branch
    
    * fix merge conflicts
    
    * fixing the leftover markdown from merge conflicts
    
    * fixing merge conflicts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import express from "express";
-
+import app from "./app";
 // Start server
 const PORT = 3000;
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/health", (_req, res) => {
-    res.json({ status: "UP", timestamp: new Date().toISOString() });
-});
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
