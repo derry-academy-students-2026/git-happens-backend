83cc19e385eb245aedc3f2ca8dfeb3bcf40d6d20
Anna Henderson <anna.henderson@kainos.com>
2026-08-07 11:33:30 +0100

    add logging messages for all levels

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "./config/morganMiddleware.js";
+import logger from "./lib/logger.js";
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.use(express.json());
 
 //morgan logging for HTTP requests
 app.use(morgan);
+
+logger.error("Error level log");
+logger.warn("This is a warning log message");
+logger.info("This is an info log message");
+logger.http("This is an HTTP log message");
+logger.debug("This is a debug log message");
 
 app.get("/health", (_req, res) => {
 	res.json({ status: "OK", timestamp: new Date().toISOString() });
