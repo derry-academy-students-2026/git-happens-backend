2b8800de69fa2f3cd87835571f4b8f059fa5ab3e
Anna Henderson <anna.henderson@kainos.com>
2026-08-11 10:49:49 +0100

    link to prisma, remove mock data

--- src/daos/jobRoleDao.ts
+++ src/daos/jobRoleDao.ts
@@ -1,12 +1,9 @@
+import prisma from "../prismaClient.js";
 import type { JobRoleModel } from "../models/jobRoleModels.js";
 
 export class JobRoleDao {
     async getJobRoles(): Promise<JobRoleModel[]> {
-        //mock data
-        return [
-            {jobRoleId: 1, roleName: "Software Engineer", location: "New York", capabilityId: "SE001", bandId: 50000, closingDate: new Date(), status: new Date()},
-            {jobRoleId: 2, roleName: "Data Scientist", location: "San Francisco", capabilityId: "DS001", bandId: 60000, closingDate: new Date(), status: new Date()},
-            {jobRoleId: 3, roleName: "Product Manager", location: "London", capabilityId: "PM001", bandId: 55000, closingDate: new Date(), status: new Date()}
-        ];
+        const jobRoles = await prisma.jobRole.findMany();
+        return jobRoles as JobRoleModel[];
     }
 }
