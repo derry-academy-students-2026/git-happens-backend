5c41ea0c96da9d3421a2c7e073d0222d949e6fd9
Cormac Mulvenna <cormac.mulvenna@kainos.com>
2026-08-26 12:00:26 +0100

    adding terraform (#18)

--- infastructure/main.tf
+++ infastructure/main.tf
@@ -19,11 +19,15 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  resource_group_name = coalesce(var.resource_group_name, "${var.project_name}-${var.environment}")
+}
+
 module "resource_group" {
   source = "./modules/resource-group"
 
-  resource_group_name = var.resource_group_name
-  location             = var.location
+  resource_group_name = local.resource_group_name
+  location            = var.location
 
   tags = merge(var.tags, {
     environment = var.environment
